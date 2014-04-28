8c6f1298e6381a0c7cd6a63120c6cfc20e471f8b
Ha Phan <thanhha.work@gmail.com>
2014-04-28 15:26:17 +0700

    PHPCS fix

--- src/Haphan/Rage4DNS/Entity/Status.php
+++ src/Haphan/Rage4DNS/Entity/Status.php
@@ -2,13 +2,26 @@
 
 namespace Haphan\Rage4DNS\Entity;
 
-
+/**
+ * Class Status encapsulates status response from RageDNS.
+ * Status response normally found in api call that only have status code or error message
+ *
+ * @package Haphan\Rage4DNS\Entity
+ */
 class Status implements TableRowInterface
 {
     private $status;
     private $id;
     private $error;
 
+
+    /**
+     * Class constructor
+     *
+     * @param string $status
+     * @param int    $id
+     * @param string $error
+     */
     public function __construct($status, $id, $error)
     {
         $this->status = $status;
@@ -16,11 +29,25 @@ class Status implements TableRowInterface
         $this->error = $error;
     }
 
+
+    /**
+     * Constructor from array. Use this to construct instance of Status from array.
+     * Internally use this to construct Status from api response.
+     *
+     * @param array $array
+     *
+     * @return Status
+     */
     public static function createFromArray($array)
     {
         return new Status($array['status'], $array['id'], $array['error']);
     }
 
+    /**
+     * Returns status
+     *
+     * @return mixed
+     */
     public function getStatus()
     {
         return $this->status;
